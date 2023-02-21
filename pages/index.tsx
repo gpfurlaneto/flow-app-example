afdeb9a9a07b4b5fa05461ad0bd3c27db0191c43
Guilherme Furlaneto <gpfurlaneto@gmail.com>
2023-02-20 23:56:43 -0300

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,39 @@
-import fs from 'fs'
-import path from 'path'
-import useCurrentUser from '../hooks/use-current-user'
-import * as fcl from "@onflow/fcl"
-import { Listing } from '../typings/Listing'
-import { authenticate } from '@onflow/fcl'
-import NFTList from '../components/NFTList'
+import fs from "fs";
+
+import * as fcl from "@onflow/fcl";
+import { authenticate } from "@onflow/fcl";
+
+import NFTList from "../components/NFTList";
+import useCurrentUser from "../hooks/use-current-user";
+import { Listing } from "../typings/Listing";
 
 export async function getServerSideProps() {
-  const getAllListingsCode = fs.readFileSync(path.join(__dirname, '../../../cadence/scripts/get_all_listings.cdc')).toString()
-  const buyItemCode = fs.readFileSync(path.join(__dirname, '../../../cadence/transactions/buy_item.cdc')).toString()
+  const getAllListingsCode = fs
+    .readFileSync("cadence/scripts/get_all_listings.cdc")
+    .toString();
+  const buyItemCode = fs
+    .readFileSync("cadence/transactions/buy_item.cdc")
+    .toString();
   return {
     props: {
       getAllListingsCode,
-      buyItemCode
+      buyItemCode,
     }, // will be passed to the page component as props
-  }
+  };
 }
 
 interface HomeProps {
-  getAllListingsCode: string
-  buyItemCode: string
+  getAllListingsCode: string;
+  buyItemCode: string;
 }
 
 export default function Home({ getAllListingsCode, buyItemCode }: HomeProps) {
-  const currentUser = useCurrentUser()
+  const currentUser = useCurrentUser();
 
   const buyItem = async (listing: Listing) => {
     try {
-      if(!currentUser?.addr){
-        await authenticate()
+      if (!currentUser?.addr) {
+        await authenticate();
       }
       const transactionId = await fcl.mutate({
         cadence: buyItemCode,
@@ -37,20 +42,23 @@ export default function Home({ getAllListingsCode, buyItemCode }: HomeProps) {
         limit: 1000,
         args: (arg: any, t: any) => [
           arg(process.env.NEXT_PUBLIC_ADDRESS, t.Address),
-          arg(listing.listingResourceID, t.UInt64)
+          arg(listing.listingResourceID, t.UInt64),
         ],
-      })
+      });
 
-      const transaction = await fcl.tx(transactionId).onceSealed()
-      return transaction.statusString === "SEALED"
-    } catch(err) {
-      console.error(err)
-      return false
+      const transaction = await fcl.tx(transactionId).onceSealed();
+      return transaction.statusString === "SEALED";
+    } catch (err) {
+      console.error(err);
+      return false;
     }
-  }
+  };
 
   return (
-    <NFTList address={process.env.NEXT_PUBLIC_ADDRESS as string} getItemsScriptCode={getAllListingsCode} buyItem={buyItem} />
-  )
+    <NFTList
+      address={process.env.NEXT_PUBLIC_ADDRESS as string}
+      getItemsScriptCode={getAllListingsCode}
+      buyItem={buyItem}
+    />
+  );
 }
- 
--- pages/my-nfts.tsx
+++ pages/my-nfts.tsx
@@ -1,35 +1,42 @@
-import fs from 'fs'
-import { useRouter } from 'next/router'
-import path from 'path'
-import { useEffect } from 'react'
-import NFTList from "../components/NFTList"
-import useCurrentUser from '../hooks/use-current-user'
+import { useEffect } from "react";
+
+import fs from "fs";
+import { useRouter } from "next/router";
+
+import NFTList from "../components/NFTList";
+import useCurrentUser from "../hooks/use-current-user";
 
 export async function getServerSideProps() {
-  const getAllListingsCode = fs.readFileSync(path.join(__dirname, '../../../cadence/scripts/get_my_nfts.cdc')).toString()
+  const getAllListingsCode = fs
+    .readFileSync("cadence/scripts/get_my_nfts.cdc")
+    .toString();
   return {
     props: {
       getAllListingsCode,
     }, // will be passed to the page component as props
-  }
+  };
 }
 
 interface MyNFTsProps {
-  getAllListingsCode: string,
+  getAllListingsCode: string;
 }
 
 export default function MyNFTs({ getAllListingsCode }: MyNFTsProps) {
-  const router = useRouter()
-  const user = useCurrentUser()
+  const router = useRouter();
+  const user = useCurrentUser();
 
   useEffect(() => {
-    if(user && !user.addr){
-      router?.push('/')
+    if (user && !user.addr) {
+      router?.push("/");
     }
-  }, [user, router])
+  }, [user, router]);
 
   return (
-    user?.addr && 
-    <NFTList address={user.addr as string} getItemsScriptCode={getAllListingsCode} />
-  )
+    user?.addr && (
+      <NFTList
+        address={user.addr as string}
+        getItemsScriptCode={getAllListingsCode}
+      />
+    )
+  );
 }
