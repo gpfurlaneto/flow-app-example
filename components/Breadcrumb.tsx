9d78a017caa799c419a347cdc013c807540deb78
Guilherme Furlaneto <gpfurlaneto@gmail.com>
2023-07-27 22:20:16 -0300

    icons

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -11,7 +11,7 @@ export default function Breadcrumb() {
   const Warning = () => {
     return (
       <span className='flex flex-row gap-2'>
-       <svg className="w-6 h-6 min-w-fit min-h-fit fill-current text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M12 5.99L19.53 19H4.47L12 5.99M12 2L1 21h22L12 2zm1 14h-2v2h2v-2zm0-6h-2v4h2v-4z"></path></svg>
+       <svg className="w-6 h-6 fill-current text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M12 5.99L19.53 19H4.47L12 5.99M12 2L1 21h22L12 2zm1 14h-2v2h2v-2zm0-6h-2v4h2v-4z"></path></svg>
         This project was tested with blocto wallet. You may experience unexpected bahavior buying with a differente wallet.
       </span>
     )
